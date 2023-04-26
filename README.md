027b71f4caa894720ec40b02992f9f1cee6f8878
SM Sohag <sohagsheik32@gmail.com>
2023-04-26 16:29:36 +0600

    update server set all data

--- README.md
+++ README.md
@@ -5,5 +5,11 @@
 # localhost link
 
 - localhost
+  ## root
   http://localhost:5000
+  ### categories
   http://localhost:5000/categories
+  #### all news
+  http://localhost:5000/news/
+  ### id wise news
+  http://localhost:5000/news/0282e0e58a5c404fbd15261f11c2ab6a
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 app.use(cors());
 app.get('/', (req, res) =>{
     res.send('Dragon News')
@@ -10,6 +11,22 @@ app.get('/', (req, res) =>{
 app.get('/categories', (req, res) => {
     res.send(categories)
 })
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+app.get('/news/:id', (req,res) =>{
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id == id)
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req,res) => {
+    // get id
+    const id = req.params.id;
+    const categoriesNews = news.filter(n => n._id == id)
+    res.send(categoriesNews);
+})
+
 app.listen(port, () =>{
    console.log(port); 
 })
